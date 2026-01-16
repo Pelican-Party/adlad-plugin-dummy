fa31d93a730473b2dd15ef06c969c84cadeeae5d
Jesper van den Ende <jespertheend@gmail.com>
2026-01-16 15:25:49 +0100

    fix: Fix configureAdLad() window not loading in firefox

--- src/createSettingsWindow.js
+++ src/createSettingsWindow.js
@@ -34,97 +34,99 @@ export function createSettingsWindow(manager) {
 		w.close();
 	});
 
-	const doc = w.document;
+	w.addEventListener("load", () => {
+		const doc = w.document;
 
-	const titleEl = doc.createElement("title");
-	titleEl.textContent = "AdLad Settings";
-	doc.head.appendChild(titleEl);
+		const titleEl = doc.createElement("title");
+		titleEl.textContent = "AdLad Settings";
+		doc.head.appendChild(titleEl);
 
-	const style = doc.createElement("style");
-	style.textContent = `
-	html {
-		font-family: Arial, Helvetica, sans-serif;
-	}
-
-	.container {
-		display: flex;
-		gap: 20px;
-		flex-direction: column;
-	}
-
-	.description {
-		color: #4f4f4f;
-		font-size: 10pt;
-		margin: 3px 0;
-	}
-	`;
-	doc.head.appendChild(style);
-
-	const settingsContainer = doc.createElement("div");
-	settingsContainer.classList.add("container");
-	doc.body.appendChild(settingsContainer);
-
-	for (const [key, config] of Object.entries(manager.settings)) {
-		const settingId = /** @type {import("./SettingsManager.js").SettingIds} */ (key);
-		const settingContainer = doc.createElement("div");
-		settingsContainer.appendChild(settingContainer);
-
-		const label = doc.createElement("label");
-		label.textContent = config.label + " ";
-		settingContainer.appendChild(label);
-
-		const value = manager.getSettingValue(settingId);
-
-		let inputEl;
-		if (config.type == "number") {
-			inputEl = doc.createElement("input");
-			inputEl.type = "number";
-			inputEl.value = String(value);
-		} else if (config.type == "boolean") {
-			inputEl = doc.createElement("input");
-			inputEl.type = "checkbox";
-			inputEl.checked = Boolean(value);
-		} else if (config.type == "enum") {
-			inputEl = doc.createElement("select");
-			if (config.options) {
-				for (const option of config.options) {
-					const el = doc.createElement("option");
-					el.textContent = option;
-					el.value = option;
-					inputEl.appendChild(el);
-				}
-			}
-			inputEl.value = String(value);
-		} else {
-			const unknownConfig = /** @type {{type: string}} */ (config);
-			throw new Error("Invalid setting type: " + unknownConfig.type);
+		const style = doc.createElement("style");
+		style.textContent = `
+		html {
+			font-family: Arial, Helvetica, sans-serif;
 		}
 
-		const certainInputEl = inputEl;
-		inputEl.addEventListener("change", () => {
-			let value;
+		.container {
+			display: flex;
+			gap: 20px;
+			flex-direction: column;
+		}
+
+		.description {
+			color: #4f4f4f;
+			font-size: 10pt;
+			margin: 3px 0;
+		}
+		`;
+		doc.head.appendChild(style);
+
+		const settingsContainer = doc.createElement("div");
+		settingsContainer.classList.add("container");
+		doc.body.appendChild(settingsContainer);
+
+		for (const [key, config] of Object.entries(manager.settings)) {
+			const settingId = /** @type {import("./SettingsManager.js").SettingIds} */ (key);
+			const settingContainer = doc.createElement("div");
+			settingsContainer.appendChild(settingContainer);
+
+			const label = doc.createElement("label");
+			label.textContent = config.label + " ";
+			settingContainer.appendChild(label);
+
+			const value = manager.getSettingValue(settingId);
+
+			let inputEl;
 			if (config.type == "number") {
-				value = parseInt(certainInputEl.value);
+				inputEl = doc.createElement("input");
+				inputEl.type = "number";
+				inputEl.value = String(value);
 			} else if (config.type == "boolean") {
-				const castInputEl = /** @type {HTMLInputElement} */ (certainInputEl);
-				value = castInputEl.checked;
+				inputEl = doc.createElement("input");
+				inputEl.type = "checkbox";
+				inputEl.checked = Boolean(value);
 			} else if (config.type == "enum") {
-				value = certainInputEl.value;
+				inputEl = doc.createElement("select");
+				if (config.options) {
+					for (const option of config.options) {
+						const el = doc.createElement("option");
+						el.textContent = option;
+						el.value = option;
+						inputEl.appendChild(el);
+					}
+				}
+				inputEl.value = String(value);
 			} else {
-				throw new Error("Unexpected setting type");
+				const unknownConfig = /** @type {{type: string}} */ (config);
+				throw new Error("Invalid setting type: " + unknownConfig.type);
 			}
-			manager.setValue(settingId, value);
-		});
 
-		const descriptionEl = doc.createElement("p");
-		descriptionEl.classList.add("description");
-		descriptionEl.innerHTML = config.description.replaceAll("\n", "<br>");
-		settingContainer.appendChild(descriptionEl);
+			const certainInputEl = inputEl;
+			inputEl.addEventListener("change", () => {
+				let value;
+				if (config.type == "number") {
+					value = parseInt(certainInputEl.value);
+				} else if (config.type == "boolean") {
+					const castInputEl = /** @type {HTMLInputElement} */ (certainInputEl);
+					value = castInputEl.checked;
+				} else if (config.type == "enum") {
+					value = certainInputEl.value;
+				} else {
+					throw new Error("Unexpected setting type");
+				}
+				manager.setValue(settingId, value);
+			});
 
-		if (config.type == "boolean") {
-			label.prepend(inputEl);
-		} else {
-			label.appendChild(inputEl);
+			const descriptionEl = doc.createElement("p");
+			descriptionEl.classList.add("description");
+			descriptionEl.innerHTML = config.description.replaceAll("\n", "<br>");
+			settingContainer.appendChild(descriptionEl);
+
+			if (config.type == "boolean") {
+				label.prepend(inputEl);
+			} else {
+				label.appendChild(inputEl);
+			}
 		}
-	}
+	});
 }
